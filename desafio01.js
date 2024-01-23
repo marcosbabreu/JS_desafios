3f4a334110bcb505cdff59409f8549021c9871cf
marcosbabreu <94197888+marcosbabreu@users.noreply.github.com>
2024-01-23 14:28:05 -0300

    Add files via upload

---
+++ desafio01.js
@@ -0,0 +1,16 @@
+/*
+Faça um programa que entre com três números, faça a média aritmética e mostre o resultado.
+*/
+
+
+<script>
+    let n1,n2,n3, resultado;
+    alert("Bem vindo (a) ao sistema de Java Script basico - Criado por Marcos");
+    n1 = prompt("Por Favor digite o Primeiro valor");
+    n2 = prompt("Por Favor digite o Segundo valor");
+    n3 = prompt("Por Favor digite o Terceiro valor");
+    resultado = Number((n1+n2+n3)/3)
+    alert(`Os valores digitados foram: ${n1}, ${n2} e ${n3} a media aritimeticas é: ${resultado} `);
+    
+
+    </script>
